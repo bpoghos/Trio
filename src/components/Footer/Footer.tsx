a5082551431adfba8c03577248c23fc5ded097cc
Poghos Barseghyan <poghosbarseghyan@Poghoss-MacBook-Air.local>
2025-03-06 14:45:22 +0400

    add skillsUp link and default be an English

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ import { FooterContent } from "../../shared/enum/enum"
 
 const Footer = () => {
 
-const navigate = useNavigate()
+  const navigate = useNavigate()
 
 
 
@@ -15,31 +15,35 @@ const navigate = useNavigate()
     <div className={style.imageContainer}>
       <img alt="footer" src={footerSVG} className={style.image} />
       <Navbar className={style.footer}>
-      <Container className={style.footerContainer}>
-        <div className={style.footerHeader}>
-          <div className={style.logoPart} >
-            <h5 onClick={() => navigate("/")}>Khachaturian Trio</h5>
-          <div className={style.websiteByContainer}>
-          <span className={style.websiteBy}>{FooterContent.WebsiteBy}</span>
-          <Nav.Link className={style.skillsUp} href="">{FooterContent.SkillsUp}</Nav.Link>
+        <Container className={style.footerContainer}>
+          <div className={style.footerHeader}>
+            <div className={style.logoPart} >
+              <h5 onClick={() => navigate("/")}>Khachaturian Trio</h5>
+              <div className={style.websiteByContainer}>
+                <span className={style.websiteBy}>{FooterContent.WebsiteBy}</span>
+                <Nav.Link
+                  className={style.skillsUp}
+                  href="https://www.skillsup.tech/"
+                  target="_blank"
+                  rel="noopener noreferrer">{FooterContent.SkillsUp}</Nav.Link>
+              </div>
+            </div>
+            <Nav className={style.menuPart}>
+              <Nav.Link className={style.link} onClick={() => navigate("/concerts")}>Concerts</Nav.Link>
+              <Nav.Link className={style.link} onClick={() => navigate("/about")}>About us</Nav.Link>
+              <Nav.Link className={style.link} onClick={() => navigate("/media")}>Media</Nav.Link>
+            </Nav>
+            <div className={style.socialPart}>
+              <Nav.Link href="https://www.facebook.com/khachaturiantrio" target="_blank" rel="noopener noreferrer">{facebookIcon}</Nav.Link>
+              <Nav.Link href='https://www.youtube.com/@khachaturyantrio' target="_blank" rel="noopener noreferrer">{youtubeIcon}</Nav.Link>
+              <Nav.Link href="https://vk.com/khachaturiantrio" target="_blank" rel="noopener noreferrer">{vkIcon}</Nav.Link>
+            </div>
           </div>
+          <div className={style.footerMidline}></div>
+          <div className={style.footerFooter}>
+            <p>{FooterContent.Copyright}</p>
           </div>
-          <Nav className={style.menuPart}>
-            <Nav.Link className={style.link} onClick={() => navigate("/concerts")}>Concerts</Nav.Link>
-            <Nav.Link className={style.link} onClick={() => navigate("/about")}>About us</Nav.Link>
-            <Nav.Link className={style.link} onClick={() => navigate("/media")}>Media</Nav.Link>
-          </Nav>
-          <div className={style.socialPart}>
-                <Nav.Link href="https://www.facebook.com/khachaturiantrio" target="_blank" rel="noopener noreferrer">{facebookIcon}</Nav.Link>
-                <Nav.Link  href='https://www.youtube.com/@khachaturyantrio' target="_blank"rel="noopener noreferrer">{youtubeIcon}</Nav.Link>
-                <Nav.Link href="https://vk.com/khachaturiantrio" target="_blank"rel="noopener noreferrer">{vkIcon}</Nav.Link>
-          </div>
-        </div>
-        <div className={style.footerMidline}></div>
-        <div className={style.footerFooter}>
-          <p>{FooterContent.Copyright}</p>
-        </div>
-      </Container>
+        </Container>
       </Navbar>
     </div>
   )
